044fa4ee3073da609fc41b4aced6e3850544e9b0
andyrich75 <arichard101@gmail.com>
2019-10-12 16:15:32 -0400

    added a different JS file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,8 +6,39 @@ var copyPassElement = document.querySelector("#copyPass");
 
 
 //prompts for crterias
-var passLength = prompt("Enter password lenght?");
+var pLength = prompt("Enter password lenght?");
+var uppercaseInput = confirm("Press ok to use uppercase letters!");
+var numbersInput = confirm("press ok to use numbers!");
+var punctuationInput = confirm("press ok to use special charaters!");
 
-//var upperCase = confirm("Press ok to use uppercase letters!");
-//var lowerCase = confirm("press ok to use lowercase letters!");
-//var specialChar = confirm("press ok to use special charaters!");
+
+//seting the variables in a string that can be used. 
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var punctuation = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+
+
+//setting the function of getting the password 
+/*function p.lenth() {
+    
+    userPassword = "";
+    passwordCharSet = "";
+
+    if (uppercaseInput.confirm) {
+      passwordCharSet += uppercase;
+    }
+    if (punctuationInput.confirm) {
+      passwordCharSet += punctuation;
+    }
+    if (numbersInput.confirm) {
+      passwordCharSet += numbers;
+    }
+    plength = Number(lengthInput.value);
+    
+//generating the pass word. 
+for (let i = 0; i < plength; i++) {
+    userPassword += passwordCharSet.charAt(Math.floor(Math.random() * p.length;
+
+    }*/
---
+++ assets/javascript/script2.js
@@ -0,0 +1,27 @@
+// asking for length of password
+var pLength = prompt("Enter password lenght?");
+    console.log(pLength);
+
+function generate(){
+
+
+   //setting the password length
+   let complexity = docuemnt.getElementById("console.log").value;
+   
+   //values that can be set 
+   let value = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+   let password = "";
+
+   //setting loop to randomize the charaters 
+   for(var i = 0; i = complexity; i++){
+       password = password + value.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
+   }
+
+   // adding the password to the text box 
+
+document.getElementById("textarea").value = password;
+
+// adding the password to clipboard.
+
+}
